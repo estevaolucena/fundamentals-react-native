ef75c6b40a67ecc271c28e1faac4529d7ae9f8dc
Estevao Lucena <estevao.lucena@somoseducacao.com.br>
2020-10-22 01:05:17 -0300

    feat: necessary implementations for succeed in tests

---
+++ README.md
@@ -0,0 +1,41 @@
+<h1 align="center">
+    <img alt="NextLevelWeek" title="#NextLevelWeek" src="https://camo.githubusercontent.com/8c13dc2618dbd7f76d1d574350b98fdee1335ce5/68747470733a2f2f726f636b6574736561742d63646e2e73332d73612d656173742d312e616d617a6f6e6177732e636f6d2f626f6f7463616d702d6865616465722e706e67" width="250px" />
+</h1>
+
+# :ledger: GoStack Bootcamp - Challange #9 - Fundamentals React Native
+
+Project purpose: https://github.com/Rocketseat/bootcamp-gostack-desafios/tree/master/desafio-database-upload
+
+## :rocket: Technologies
+This project was developed with the following technologies:
+- React Native
+- Jest
+- Yarn
+
+## :runner: Running
+
+```bash
+# Clone this repository
+git clone https://github.com/estevaolucena/fundamentals-react-native.git
+
+# Go into the project folder
+cd fundamentals-react-native
+
+# Install dependencies
+yarn install
+
+# Run the fake API
+yarn json-server server.json -p 3333
+
+# Run the project
+yarn start
+
+# In another shell, run the build to emulate
+yarn android
+
+# Run the test suit
+yarn test
+```
+
+
+Made with ♥ by Estevão Lucena :wave: [Get in touch!](https://www.linkedin.com/in/estevaolucena/)
--- src/hooks/cart.tsx
+++ src/hooks/cart.tsx
@@ -30,23 +30,85 @@ const CartProvider: React.FC = ({ children }) => {
 
   useEffect(() => {
     async function loadProducts(): Promise<void> {
-      // TODO LOAD ITEMS FROM ASYNC STORAGE
+      const item = await AsyncStorage.getItem('@gomarketplace:products');
+
+      if (item) {
+        setProducts([...JSON.parse(item)]);
+      }
     }
 
     loadProducts();
   }, []);
 
-  const addToCart = useCallback(async product => {
-    // TODO ADD A NEW ITEM TO THE CART
-  }, []);
+  const addToCart = useCallback(
+    async product => {
+      const item = products.find(p => p.id === product.id);
 
-  const increment = useCallback(async id => {
-    // TODO INCREMENTS A PRODUCT QUANTITY IN THE CART
-  }, []);
+      if (item) {
+        setProducts(
+          products.map(p =>
+            p.id === product.id ? { ...product, quantity: p.quantity + 1 } : p,
+          ),
+        );
+      } else {
+        setProducts([...products, { ...product, quantity: 1 }]);
+      }
 
-  const decrement = useCallback(async id => {
-    // TODO DECREMENTS A PRODUCT QUANTITY IN THE CART
-  }, []);
+      await AsyncStorage.setItem(
+        '@gomarketplace:products',
+        JSON.stringify(products),
+      );
+    },
+    [products],
+  );
+
+  const increment = useCallback(
+    async id => {
+      const item = products.find(p => p.id === id);
+
+      if (item) {
+        item.quantity += 1;
+
+        await AsyncStorage.setItem(
+          '@gomarketplace:products',
+          JSON.stringify([...products]),
+        );
+
+        setProducts([...products]);
+      }
+    },
+    [products],
+  );
+
+  const decrement = useCallback(
+    async id => {
+      const item = products.find(p => p.id === id);
+
+      if (item && item.quantity > 1) {
+        item.quantity -= 1;
+
+        await AsyncStorage.setItem(
+          '@gomarketplace:products',
+          JSON.stringify([...products]),
+        );
+
+        return setProducts([...products]);
+      }
+
+      products.splice(
+        products.findIndex(p => p.id === id),
+        1,
+      );
+
+      await AsyncStorage.setItem(
+        '@gomarketplace:products',
+        JSON.stringify([...products]),
+      );
+
+      return setProducts([...products]);
+    },
+    [products],
+  );
 
   const value = React.useMemo(
     () => ({ addToCart, increment, decrement, products }),
